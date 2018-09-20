fd3f0a80ef44f29b6013a5ed6105647394ccc982
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-09-20 08:54:21 -0400

    Made most of the code.

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -8,15 +8,28 @@ public class NumberGuessingGame {
 
         Scanner keyboard;
         int pickedNumber;
+        int guessedNumber;
 
         pickedNumber = (int)(Math.random()*100);
+        keyboard = new Scanner(System.in);
 
         System.out.println(pickedNumber);
+        System.out.println("Please guess a number.");
+        guessedNumber = keyboard.nextInt();
 
 
-
-        if (pickedNumber <50){
-
+        if (guessedNumber<pickedNumber){
+            System.out.println("Your number is too low.");
+            System.out.println("Please guess again.");
+            guessedNumber = keyboard.nextInt();
+        }
+        else if (guessedNumber>pickedNumber){
+            System.out.println("You number is too high.");
+            System.out.println("Please guess again.");
+            guessedNumber = keyboard.nextInt();
+        }
+        else if (guessedNumber==pickedNumber){
+            System.out.println("You guessed right! Congratulations!");
         }
 
     }
