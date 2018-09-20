f18b927672198b330623cf240d20e5a6642eb6bc
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-09-20 08:04:03 -0400

    More variables and stuff

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class NumberGuessingGame {
+
+
+    public static void main(String[]args){
+
+
+        Scanner keyboard;
+        int pickedNumber;
+
+        pickedNumber = (int)(Math.random()*100);
+
+        System.out.println(pickedNumber);
+
+
+
+        if (pickedNumber <50){
+
+        }
+
+    }
+
+
 }
