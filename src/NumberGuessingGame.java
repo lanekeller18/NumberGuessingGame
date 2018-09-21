b92a1fa27e4e650f4fba38bcee9cd0dd6bb48802
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-09-21 08:01:36 -0400

    Added the loop

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -9,28 +9,37 @@ public class NumberGuessingGame {
         Scanner keyboard;
         int pickedNumber;
         int guessedNumber;
+        boolean gotItRight;
 
         pickedNumber = (int)(Math.random()*100);
         keyboard = new Scanner(System.in);
+        gotItRight = false;
 
-        System.out.println(pickedNumber);
+        //System.out.println(pickedNumber);
         System.out.println("Please guess a number.");
         guessedNumber = keyboard.nextInt();
 
+        while(!gotItRight) {
 
-        if (guessedNumber<pickedNumber){
-            System.out.println("Your number is too low.");
+
+            if (guessedNumber < pickedNumber) {
+                System.out.println("Your number is too low.");
+
+            } else if (guessedNumber > pickedNumber) {
+                System.out.println("You number is too high.");
+
+            } else if (guessedNumber == pickedNumber) {
+                System.out.println("You guessed right! Congratulations!");
+
+                 gotItRight = true;
+                 break;
+            }
+
             System.out.println("Please guess again.");
             guessedNumber = keyboard.nextInt();
+
         }
-        else if (guessedNumber>pickedNumber){
-            System.out.println("You number is too high.");
-            System.out.println("Please guess again.");
-            guessedNumber = keyboard.nextInt();
-        }
-        else if (guessedNumber==pickedNumber){
-            System.out.println("You guessed right! Congratulations!");
-        }
+        //System.out.println("I am now below the while loop");
 
     }
 
