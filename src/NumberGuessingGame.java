8d055522c6c966e126827340af2dc903433d2c79
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-09-28 07:30:13 -0400

    Added new things

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -3,45 +3,52 @@ import java.util.Scanner;
 public class NumberGuessingGame {
 
 
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
 
         Scanner keyboard;
         int pickedNumber;
         int guessedNumber;
         boolean gotItRight;
+        int ;
 
-        pickedNumber = (int)(Math.random()*100);
+        pickedNumber = (int) (Math.random() * 100);
         keyboard = new Scanner(System.in);
         gotItRight = false;
 
+
+
         //System.out.println(pickedNumber);
-        System.out.println("Please guess a number.");
-        guessedNumber = keyboard.nextInt();
 
-        while(!gotItRight) {
-
-
-            if (guessedNumber < pickedNumber) {
-                System.out.println("Your number is too low.");
-
-            } else if (guessedNumber > pickedNumber) {
-                System.out.println("You number is too high.");
-
-            } else if (guessedNumber == pickedNumber) {
-                System.out.println("You guessed right! Congratulations!");
-
-                 gotItRight = true;
-                 break;
-            }
-
-            System.out.println("Please guess again.");
+            System.out.println("Please guess a number.");
             guessedNumber = keyboard.nextInt();
 
-        }
-        //System.out.println("I am now below the while loop");
+            while (!gotItRight) {
 
+
+                if (guessedNumber < pickedNumber) {
+                    System.out.println("Your number is too low.");
+
+                } else if (guessedNumber > pickedNumber) {
+                    System.out.println("You number is too high.");
+
+                } else if (guessedNumber == pickedNumber) {
+                    System.out.println("You guessed right! Congratulations!");
+
+                    gotItRight = true;
+                    break;
+                }
+
+                System.out.println("Please guess again.");
+                guessedNumber = keyboard.nextInt();
+
+            }
+            //System.out.println("I am now below the while loop");
+
+
+
+
+
+        }
     }
 
-
-}
